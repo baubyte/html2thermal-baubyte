cc54d84bde00ddbe74bb9bcdf538fe366f3114bb
baubyte <paredbaez.martin@gmail.com>
2024-10-02 20:42:45 -0300

    fix error params

--- src/execute.js
+++ src/execute.js
@@ -6,7 +6,7 @@ module.exports = async (printer, html, isCut = true) => {
   for (let i = 0; i < commands.length; i++) {
     const { name, data, isAwait, isArrayData, ...attributes } = commands[i];
     if (typeof printer[name] === 'function') {
-      (isArrayData) ? await printer[name](data) : await printer[name](data);
+      (isArrayData) ? await printer[name](...data) : await printer[name](data, attributes);
     } else {
       console.warn(`Comando ${name} no reconocido.`);
     }
